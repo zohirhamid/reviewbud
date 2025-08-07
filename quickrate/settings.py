7f314e8282b5b4daa478bf5cc4e11239f224739c
zohir hamid <zohirhamid011@gmail.com>
2025-08-08 00:30:45 +0100

    added google login and kept email login too
    - set up google oauth with allauth
    
    - updated login and signup pages
    
    - fixed the css for the new buttons

--- quickrate/settings.py
+++ quickrate/settings.py
@@ -9,6 +9,9 @@ SECRET_KEY = 'django-insecure-gx_!+o%_@jo9+%(1f+4uiyh24$wj50ghwiz5=l(jpv9=&ah+=*
 DEBUG = True
 ALLOWED_HOSTS = []
 
+# Application definition
+SITE_ID = 1
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -18,7 +21,23 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'businesses',
     'reviews',
+
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.google'
 ]
+
+SOCIALACCOUNTS_PROVIDERS = {
+    'google': {
+        'SCOPE': [
+            'profile',
+            'email',
+        ],
+        'AUTH_PARAMS': {'access_type': 'online'}
+    }
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -26,6 +45,7 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'allauth.account.middleware.AccountMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
@@ -71,10 +91,16 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend',  # ✅ Correct
+]
+
+SOCIALACCOUNT_LOGIN_ON_GET = True
+
 # Login settings
-LOGIN_REDIRECT_URL = '/businesses/dashboard/'
+LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
-LOGIN_URL = '/businesses/login/'
 
 # API Configuration - Remove the duplicate OPENAI_API_KEY line
 OPENAI_API_KEY = config('OPENAI_API_KEY', default='')
