58610ef87d7fc67204f249d77ea4ceade29c2707
zohir hamid <zohirhamid011@gmail.com>
2026-01-07 14:55:02 +0000

    Remove unused files and cleanup deprecated project artifacts

--- reviewbud/celery.py
+++
@@ -1,8 +0,0 @@
-import os
-from celery import Celery
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE","reviewbud.settings")
-
-celery_app = Celery("reviewbud")
-celery_app.config_from_object("django.conf:settings", namespace="CELERY")
-celery_app.autodiscover_tasks()
